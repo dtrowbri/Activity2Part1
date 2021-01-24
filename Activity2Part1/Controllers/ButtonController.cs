b4aa7d22444755b988527cf19253c2aadb19c19e
Donald_Trowbridge <dtrowbridg@my.gcu.edu>
2021-01-23 19:00:55 -0700

    Add project files.

---
+++ Activity2Part1/Controllers/ButtonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Activity2Part1.Models;
+
+namespace Activity2Part1.Controllers
+{
+    public class ButtonController : Controller
+    {
+        static List<ButtonModel> buttons = new List<ButtonModel>();
+        // GET: Button
+        public ActionResult Index()
+        {
+            buttons.Add(new ButtonModel(true));
+            buttons.Add(new ButtonModel(false));
+
+            return View("Button", buttons);
+        }
+
+        public ActionResult OnButtonClick(string mine)
+        {
+            if(mine == "1")
+            {
+                buttons[0].State = !buttons[0].State;
+            } else if(mine == "2")
+            {
+                buttons[1].State = !buttons[1].State;
+            }
+            return View("Button", buttons);
+        }
+    }
+}
---
+++ Activity2Part1/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Activity2Part1.Models;
+
+namespace Activity2Part1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<UserModel> customers = new List<UserModel>();
+            customers.Add(new UserModel("John Smith", "johnsmith@gmail.com", "602-867-5309"));
+            customers.Add(new UserModel("Fake Name", "fakename@yahoo.com", "480-123-4567"));
+            customers.Add(new UserModel("Steve Fisher", "outfishing@outlook.com", "432-346-3426"));
+            customers.Add(new UserModel("Homer Simpson", "doh@gmail.com", "432-978-9372"));
+            customers.Add(new UserModel("Stupid Flanders", "leftorium@yahoo.com", "978-932-0738"));
+
+            return View("Test", customers);
+        }
+    }
+}
